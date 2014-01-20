e03ec21f3d11cb007cbe56e5386c5d88a1dab45a
RoboHawks <RoboHawks@10.0.1.62>
2014-01-20 13:41:34 -0500

    Added Math.pow

--- FRC2014/src/edu/wpi/first/wpilibj/templates/NewMath.java
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/NewMath.java
@@ -11,9 +11,17 @@ package edu.wpi.first.wpilibj.templates;
  * @author Jamie
  */
 public class NewMath {
-    int test;
-    public double toTheThird(double input){
-        return input*input*input;
+    /****
+     * @param a base number
+     * @param b exponent
+     * @return returns a to the b power
+     */
+    public double pow(double a, double b){
+        double base = a;
+        for (int i = 0; i < b-1; i++){
+            a *= base;
+        }
+        return a;
     }
-    
+
 }
