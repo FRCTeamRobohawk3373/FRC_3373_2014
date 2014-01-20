9bfe8da92cb42c6aba80641206ee39479794913d
RoboHawks <RoboHawks@10.0.1.62>
2014-01-20 13:41:24 -0500

    Wrote drive class

--- FRC2014/src/edu/wpi/first/wpilibj/templates/Drive.java
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/Drive.java
@@ -6,6 +6,8 @@
 
 package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.templates.NewMath;
+import edu.wpi.first.wpilibj.templates.Deadband;
 /**
  *
  * @author Jamie
@@ -17,13 +19,46 @@ public class Drive {
         return instance;
     }
     
+    NewMath math = new NewMath(); 
     RobotDrive mechanum = new RobotDrive(1,2,3,4);
+    Deadband deadband = new Deadband();
+    double speed = .50; //default movement speed
+    /****
+     * @param driveLX X axis of the left joystick on the drive controller
+     * @param driveRX X Axis of the right joystick on the drive controller
+     * @param driveLY Y axis of the left joystick on the drive controller
+     */
+    public void drive(double driveLX, double driveRX, double driveLY){
+        mechanum.mecanumDrive_Cartesian(math.pow(deadband.zero(driveLX, .1), 3)*speed,
+                math.pow(deadband.zero(driveRX, .1), 3)*speed,
+                math.pow(deadband.zero(driveLY, .1), 3)*speed, 0);
+    }
     
-    public void orienationSwitcher(boolean isShooterFront){
-        if (isShooterFront){ //shooting direction is front
-            mechanum.mecanumDrive_Cartesian(1, 2, 1, 0);
+    public void orientationSwitcher(boolean isTrueFrontRequested){
+        boolean isShooterFront = true;
+        if (isTrueFrontRequested && isShooterFront){
+            isShooterFront = false;
+        } else if (isTrueFrontRequested && !isShooterFront){
+            isShooterFront = false;
+        }
+        if (isShooterFront){
+            speed = Math.abs(speed);
         } else {
-            mechanum.mecanumDrive_Cartesian(-1, -2, -1,0);
+            speed = -Math.abs(speed);
+        }
+    }
+    
+    public void speedModifier(boolean sniperButton, boolean turboButton){
+        if (sniperButton) {
+            speed *= .5;
+        }
+        
+        if (turboButton) {
+            speed *= 2;
+        }
+        
+        if (!turboButton && !sniperButton){
+            speed = .5;
         }
     }
 }
