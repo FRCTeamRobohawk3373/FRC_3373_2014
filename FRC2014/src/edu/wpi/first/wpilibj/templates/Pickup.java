ef94956b5883b5f3ccc010e689bfd7e2de08493b
RoboHawks <RoboHawks@10.0.1.102>
2014-01-25 13:30:12 -0500

    added a way to go to a specific angle, or to continuously move to a spot

--- FRC2014/src/edu/wpi/first/wpilibj/templates/Pickup.java
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/Pickup.java
@@ -6,10 +6,60 @@
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  *
  * @author Jamie
  */
 public class Pickup {
+    AnalogChannel pickupPot = new AnalogChannel(1); //Voltage from one to five
+    Talon actuateTalon = new Talon(1);
+    Deadband deadband = new Deadband();
     
+    double maxAngle = 120;
+    double maxVoltage = 5;
+    final double angleTolerance = 3;
+    double diagSpeed = .5;
+    
+    public void goToPos(final double pos, final double speed){
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                while (deadband.zero(Math.abs(getPickupPos() - pos), angleTolerance, 1)){
+                    if (deadband.zero(Math.abs(getPickupPos() - pos), angleTolerance, 1)){
+                        goToAngle(pos, getPickupPos(), speed);
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+    
+    public double getPickupPos(){
+        double angle = pickupPot.getVoltage() * (maxAngle/maxVoltage);
+        return angle;
+    }
+    
+    public void goToAngle(double target, double currentPos, double speed){
+        if (target < currentPos){
+            actuateTalon.set(speed);
+        } else if (target > currentPos){
+            actuateTalon.set(-speed);
+        } else {
+            actuateTalon.set(0);
+        }
+    }
+    
+    public double angleDiag(boolean up, boolean down){
+        if (up && !down){
+            actuateTalon.set(diagSpeed);
+        } else if (down && !up){
+            actuateTalon.set(-diagSpeed);
+        } else {
+            actuateTalon.set(0);
+        }
+        return getPickupPos();
+    }
 }
