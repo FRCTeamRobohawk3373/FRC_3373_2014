20373bc3817d0584c821bfac6fed654287ed9c56
RoboHawks <RoboHawks@10.0.1.102>
2014-01-26 18:03:00 -0500

    Added code to allow the cRio to tlak to an external server

--- FRC2014/src/edu/wpi/first/wpilibj/templates/PiSocket.java
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/PiSocket.java
@@ -3,19 +3,71 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import javax.microedition.io.Connector;
 import javax.microedition.io.SocketConnection;
+import javax.microedition.io.StreamConnection;
+
 /**
  *
  * @author RoboHawks
  */
 public class PiSocket {
-        //Socket myClient = null;
-        DataOutputStream os = null;
-        DataInputStream is = null;
+
+    SocketConnection connection;
+    DataOutputStream os = null;
+    DataInputStream is = null;
+    boolean isReceiveThreadRunning = false;
+    static char serverChar;
+    boolean isConnected = false;
+
+    public void connect() {
+        try {
+            connection = (SocketConnection) Connector.open("socket://10.33.73.5:3500", Connector.READ_WRITE, true);
+            os = connection.openDataOutputStream();
+            is = connection.openDataInputStream();
+            isConnected = true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            isConnected = false;
+        }
+    }
+
+    public void disconnect() throws IOException {
+        is.close();
+        os.close();
+        connection.close();
+        isConnected = false;
+
+    }
+
+    public void sendString() throws IOException {
+        os.writeChars("Test");
+
+    }
+
+    public char receiveString() throws IOException {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                isReceiveThreadRunning = true;
+                try {
+                    char threadChar = is.readChar();
+                    serverChar = threadChar;
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                isReceiveThreadRunning = false;
+            }
+        });
+        if (!isReceiveThreadRunning) {
+            thread.start();
+        }
         
+        return serverChar;
+    }
+
 }
