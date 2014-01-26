5a42ff2bd90f347be9930e646ebdef7fe2c2c30a
RoboHawks <RoboHawks@10.0.1.102>
2014-01-26 15:25:19 -0500

    added threaded method to control shooting with SmartDashboard-influenced charge time

--- FRC2014/src/edu/wpi/first/wpilibj/templates/Launcher.java
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/Launcher.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
+
 import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -12,49 +12,85 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.templates.Diagnostics;
 import edu.wpi.first.wpilibj.smartdashboard.*;
+
 /**
  *
  * @author Jamie
  */
 public class Launcher {
-    
-    DoubleSolenoid lockingSolenoid = new DoubleSolenoid(1,2);//currently in solenoid port 1 for testing
+
+    DoubleSolenoid lockingSolenoid = new DoubleSolenoid(1, 2);//currently in solenoid port 1 for testing
     Solenoid pressureSolenoid = new Solenoid(3);
     Solenoid exhaustSolenoid = new Solenoid(4);
-    
+    Solenoid testLockingSolenoid = new Solenoid(5);
+
     DigitalInput isPistonHome = new DigitalInput(1); //curently in I/O port 1 for testing
-    
+
     AnalogChannel pressureSensor = new AnalogChannel(1);
-    
+
     boolean isReadyToShoot;
     double currentPressure;
     boolean isThreadRunning = false;
-    
-    public void unlockShootingPistons(){
+    boolean isShootThreadRunning = false;
+
+    public void unlockShootingPistons() {
         lockingSolenoid.set(DoubleSolenoid.Value.kForward); //false is allowing the pisotn to retract releasing the shooting piston
     }
-    public void lockShootingPistons(){
-        if(isPistonHome.get()){
+
+    public void lockShootingPistons() {
+        if (isPistonHome.get()) {
             lockingSolenoid.set(DoubleSolenoid.Value.kReverse); //true is allowing the piston to extend locking the shooting pistions
         }
-    }    
-    public boolean chargeShootingPistons(final double targetPressure){
+    }
+
+    public boolean chargeShootingPistons(final double targetPressure) {
         final Thread thread = new Thread(new Runnable() {
-           public void run(){
-               isThreadRunning = true;
-               while(targetPressure > currentPressure){
-                   currentPressure = pressureSensor.getValue(); //we want to change this .getValue(that returns a voltage) to instead return pressure in psi
-                   pressureSolenoid.set(true);
+            public void run() {
+                isThreadRunning = true;
+                while (targetPressure > currentPressure) {
+                    currentPressure = pressureSensor.getValue(); //we want to change this .getValue(that returns a voltage) to instead return pressure in psi
+                    pressureSolenoid.set(true);
                 }
-               while(targetPressure < currentPressure){
-                   currentPressure = pressureSensor.getValue(); //we want to change this .getValue(that returns a voltage) to instead return pressure in psi 
-                   exhaustSolenoid.set(true);
-               }
+                while (targetPressure < currentPressure) {
+                    currentPressure = pressureSensor.getValue(); //we want to change this .getValue(that returns a voltage) to instead return pressure in psi 
+                    exhaustSolenoid.set(true);
+                }
             }
-            });
-        if(!isThreadRunning){
+        });
+        if (!isThreadRunning) {
             thread.start();
         }
         return isReadyToShoot;
-        }            
- }
+    }
+
+    public void shootThread() {
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("In thread");
+                isShootThreadRunning = true;
+                testLockingSolenoid.set(false);
+                pressureSolenoid.set(true);
+                try {
+                    long shootDelay = (long) SmartDashboard.getNumber("Shoot Delay");
+                    System.out.println(shootDelay);
+                    Thread.sleep(shootDelay);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                testLockingSolenoid.set(true);
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                pressureSolenoid.set(false);
+                isShootThreadRunning = false;
+                System.out.println(isShootThreadRunning);
+            }
+        });
+        if (!isShootThreadRunning) {
+            thread.start();
+        }
+
+    }
+}
