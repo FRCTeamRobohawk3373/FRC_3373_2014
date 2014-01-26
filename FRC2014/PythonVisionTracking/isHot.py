4c8007c790c73db029f6b30a67931825b431ec1c
Nick <nick@NickUbuntu.(none)>
2014-01-26 16:47:33 -0500

    tells if goal is hot, finds distance in pixels

--- FRC2014/PythonVisionTracking/isHot.py
+++ FRC2014/PythonVisionTracking/isHot.py
@@ -2,47 +2,68 @@
 
 import SimpleCV
 
+DEBUG = True
+
 screenSize = (800, 600)
 display = SimpleCV.Display(screenSize)
 
 camURL = "http://192.168.0.90:80/jpg/image.jpg"
 
-isHot = False
-ratioLow = 5.0
-ratioHigh = 6.0
+ratioLow = 1.1
+ratioHigh = 1.6
 
-while display.isNotDone():
+def main():
+    isHot = False
+    while display.isNotDone():
 
-    drawingLayer = SimpleCV.DrawingLayer(screenSize)
+        drawingLayer = SimpleCV.DrawingLayer(screenSize)
 
-    img = SimpleCV.Image(camURL)
+        img = SimpleCV.Image(camURL)
 
-    greenDist = img.colorDistance(SimpleCV.Color.AQUAMARINE)
-    filtered = img - greenDist
+        greenDist = img.colorDistance(SimpleCV.Color.AQUAMARINE)
+        filtered = img - greenDist
 
-    blobs = filtered.findBlobs()#thresheval = 80, minsize = 300
+        blobs = filtered.findBlobs()#thresheval = 80, minsize = 300
 
-    if blobs:
-        rectangles = blobs.filter([b.isRectangle(0.3) for b in blobs])
-        if rectangles:
-            
-            debugPrint()
+        if blobs:
+            rectangles = blobs.filter([b.isRectangle(0.3) for b in blobs])
+            if rectangles:
+                
+                drawRects(rectangles, filtered, drawingLayer)
+                isHot = checkIsHot(rectangles, isHot)
+                if DEBUG: debugPrint(rectangles, isHot)
 
-            drawingLayer.centeredRectangle(rectangles[-1].coordinates(), (rectangles[-1].width(), rectangles[-1].height()), SimpleCV.Color.YELLOW, 1, False, -1)
-            filtered.addDrawingLayer(drawingLayer)
-            filtered.applyLayers()
+                filtered.applyLayers()
 
-            
-            #TODO Fix the problem when there is only one rectangle in the array
-            #if ((rectangles[-1].height() * rectangles[-2].width()) > ratioLow) and ((rectangles[-1].height() * rectangles[-2].width()) < ratioHigh):
-            #    isHot = True
-            #else:
-                isHot = False
-    filtered.save(display)
+        filtered.save(display)
 
 
-def debugPrint():
-    print(rectangles[-1].height() * rectangles[-2].width())
-    print(isHot)
+def debugPrint(rectangles, isHot):
+    if len(rectangles) > 1:
+        rec1H = float(rectangles[-1].height())
+        rec2W = float(rectangles[-2].width())
+        print("ratio: " + str(rec1H / rec2W))
+    print("isHot: " + str(isHot))
+    print("distance: " + str(rectangles[-1].height()))
 
-   
+def drawRects(rectangles, filtered, drawingLayer):
+    drawingLayer.centeredRectangle(rectangles[-1].coordinates(), (rectangles[-1].width(), rectangles[-1].height()), SimpleCV.Color.YELLOW, 1, False, -1)
+    if len(rectangles) > 1:
+        drawingLayer.centeredRectangle(rectangles[-2].coordinates(), (rectangles[-2].width(), rectangles[-2].height()), SimpleCV.Color.YELLOW, 1, False, -1) 
+    filtered.addDrawingLayer(drawingLayer)
+
+def checkIsHot(rectangles, isHot):
+    if len(rectangles) > 1:
+        rec1H = float(rectangles[-1].height())
+        rec2W = float(rectangles[-2].width())
+        ratio = rec1H / rec2W
+
+        isHot = (ratio > ratioLow) and (ratio < ratioHigh)
+        #print("isHot live: " + str(isHot))
+    else:
+        isHot = False
+
+    return isHot
+
+main()
+
