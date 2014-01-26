6b1aae912d6d287ebad50a1c568dda0ce510ee25
Nick <nick@NickUbuntu.(none)>
2014-01-26 17:38:10 -0500

    now has a test server and client

--- FRC2014/Python/communication.py
+++ FRC2014/Python/communication.py
@@ -1,6 +1,43 @@
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+
+def client():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+
+    print "received data:", data
+
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        print "received data:", data
+        conn.send(data)  # echo
+        conn.close()
+
 def restart():
     command = "/usr/bin/sudo /sbin/shutdown -r now"
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print output
+
+#!/usr/bin/env python
+
+
+server()
