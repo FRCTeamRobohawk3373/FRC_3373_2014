dd12dd00b3b6931ac9ddbf9e622350b1a0a9026f
Philip Mulford <peragorefalith1@gmail.com>
2014-02-01 19:47:53 -0500

    added diag

--- FRC2014/src/edu/wpi/first/wpilibj/templates/Diagnostics.java
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/Diagnostics.java
@@ -16,18 +16,39 @@ public class Diagnostics { //provides diagnostic methods to print out problems
     public static Diagnostics getInstance(){
         return instance;
     }
-    
+    /**
+     * Use this to display PSI on smartDashboard
+     * @param psi PSI measure from sensor
+     * @param meterID ID of the meter as a String
+     * @return returns String of PSI and the ID
+     */
     public String showPSI(double psi, String meterID){
         SmartDashboard.putNumber("PSI " + meterID, psi);
         return ("PSI " + meterID);
     }
-    
+    /**
+     * 
+     * @param motorSpeed speed of motor to be fed in
+     * @param motorID ID of motor
+     * @return returns a string with The Motor ID identified 
+     */
     public String showMotorSpeed(double motorSpeed, String motorID){
         SmartDashboard.putNumber("Motor " + "" + motorID + "" + " Speed", motorSpeed);
         return ("Motor " + "" + motorID + "" + " Speed");
     }
-    
+    /**
+     * 
+     * @param potMeasure
+     * @param potID 
+     */
     public void showPotMeasure(double potMeasure, String potID){
         SmartDashboard.putNumber("Pot " + potID + " Measure", potMeasure);
+    }
+    /**
+     * 
+     * @param isCharged 
+     */
+    public void showChargeStatus(boolean isCharged){
+        SmartDashboard.putBoolean("IsCharged: ", isCharged);
     }
 }
