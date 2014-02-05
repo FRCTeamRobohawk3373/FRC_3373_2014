ad90e905db0a26f77058349dbf7519a7a444a1ef
Nick <nick@NickUbuntu.(none)>
2014-02-05 18:32:27 -0500

    Communicates With CRio

--- FRC2014/Python/communication.py
+++ FRC2014/Python/communication.py
@@ -1,40 +1,65 @@
 import socket
+import unicodedata
+import re
+import thread
+import time
 
 
 TCP_IP = 'localhost'
 TCP_PORT = 3373
 BUFFER_SIZE = 1024
 
+isHot = True
+distance = 23.4
+
 def server():
     #Start the Server
+    print("Starting Server")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 3373)
     s.bind(('', TCP_PORT))
     s.listen(1)
     
     #Pause and Wait for Connection
+    print("Waiting for Connection...")
     conn, addr = s.accept()
-    print 'Connection address:', addr
+    print 'Connected with:', addr
     
     #Recieve and Send Data 
     while (True):
-        print("Waiting For Data")
+        print("Waiting For Data...")
         data = conn.recv(BUFFER_SIZE)
+        
+        if not data: break
+        
         print('Received Data: ' + data)
 
-        if not data: break
-
-        if len(data) >= 1:
-            if data[0] == "@":
-                #Parse Data
-                command = data.replace("@", "")
-            
-                #Do Things Here!!
-                print("Command: " + command)
-                response = "@" + command + "@\n" #echo
+        if data[0] == "@":
+            #Parse Data
+            data = unicode(data)
+            data = data.replace("@", "")
+            data = data.replace("\n", "")
+            command = re.sub(r'[^a-zA-Z0-9]','', data)
                 
-                #Respond
-                print('Sending: ' + response)
-                conn.send(response.encode("utf-16"))
+            #Make Response
+            print("Command: " + command)
+            response = ""
+            
+            if unicodedata.normalize("NFD", unicode(command)) == unicodedata.normalize("NFD", unicode("ISHOT")):
+                response = str(isHot)
+            elif unicodedata.normalize("NFD", unicode(command)) == unicodedata.normalize("NFD", unicode("DISTANCE")):
+                response = str(distance)
+            else:
+                response = "INVALID"
+                
+            #Format Response    
+            response = "@" + response + "@\n"
+            
+            #Respond
+            print('Sending: ' + response)
+            conn.send(response.encode("utf-16"))
+            
+    print("Connection Closed")
     
 def shutdown():
     command = "/usr/bin/sudo /sbin/shutdown -r now"
@@ -42,5 +67,12 @@ def shutdown():
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print output
+    
+def main():
+        thread.start_new_thread(server, ())
+        while(1):
+            time.sleep(1)
+            
+main()
 
-server()
+#server()
--- FRC2014/Python/isHot.py
+++ FRC2014/Python/isHot.py
@@ -1,20 +1,33 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import SimpleCV
+import re
+import thread
+import socket
+import unicodedata
+import time
 
 DEBUG = True
 
-screenSize = (800, 600)
-if DEBUG: display = SimpleCV.Display(screenSize)
+TCP_IP = 'localhost'
+TCP_PORT = 3373
+BUFFER_SIZE = 1024
 
 camURL = "http://10.33.73.167:80/jpg/image.jpg"
 
 ratioLow = 1.1
 ratioHigh = 1.6
 
+screenSize = (800, 600)
+if DEBUG: display = SimpleCV.Display(screenSize)
+
+isHot = False
+distance = 0
+
 def main():
-    isHot = False
-    distance = 0
+	
+	thread.start_new_thread(server, ())
+    
     while (DEBUG and display.isNotDone()) or (DEBUG != True):
 
         img = SimpleCV.Image(camURL)
@@ -28,19 +41,19 @@ def main():
             rectangles = blobs.filter([b.isRectangle(0.3) for b in blobs])
             if rectangles:
                 
-                isHot = checkIsHot(rectangles, isHot)
-                distance = getDistance(rectangles)
+                checkIsHot(rectangles, isHot)
+                getDistance(rectangles)
 
                 if DEBUG:
                     drawingLayer = SimpleCV.DrawingLayer(screenSize)
                     drawRects(rectangles, filtered, drawingLayer)
-                    debugPrint(rectangles, isHot, distance)
+                    debugPrint(rectangles)
                     filtered.applyLayers()
                     
         if DEBUG: filtered.save(display)
 
 
-def debugPrint(rectangles, isHot, distance):
+def debugPrint(rectangles):
     if len(rectangles) > 1:
         rec1H = float(rectangles[-1].height())
         rec2W = float(rectangles[-2].width())
@@ -54,7 +67,7 @@ def drawRects(rectangles, filtered, drawingLayer):
         drawingLayer.centeredRectangle(rectangles[-2].coordinates(), (rectangles[-2].width(), rectangles[-2].height()), SimpleCV.Color.YELLOW, 1, False, -1) 
     filtered.addDrawingLayer(drawingLayer)
 
-def checkIsHot(rectangles, isHot):
+def checkIsHot(rectangles):
     if len(rectangles) > 1:
         rec1H = float(rectangles[-1].height())
         rec2W = float(rectangles[-2].width())
@@ -64,9 +77,56 @@ def checkIsHot(rectangles, isHot):
     else:
         isHot = False
 
-    return isHot
-
 def getDistance(rectangles):
-    return str(rectangles[-1].height())
+    distance = str(rectangles[-1].height())
+    
+def server():
+    #Start the Server
+    print("Starting Server")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 3373)
+    s.bind(('', TCP_PORT))
+    s.listen(1)
+    
+    #Pause and Wait for Connection
+    print("Waiting for Connection...")
+    conn, addr = s.accept()
+    print 'Connected with:', addr
+    
+    #Recieve and Send Data 
+    while (True):
+        print("Waiting For Data...")
+        data = conn.recv(BUFFER_SIZE)
+        
+        if not data: break
+        
+        print('Received Data: ' + data)
+
+        if data[0] == "@":
+            #Parse Data
+            data = unicode(data)
+            data = data.replace("@", "")
+            data = data.replace("\n", "")
+            command = re.sub(r'[^a-zA-Z0-9]','', data)
+                
+            #Make Response
+            print("Command: " + command)
+            response = ""
+            
+            if unicodedata.normalize("NFD", unicode(command)) == unicodedata.normalize("NFD", unicode("ISHOT")):
+                response = str(isHot)
+            elif unicodedata.normalize("NFD", unicode(command)) == unicodedata.normalize("NFD", unicode("DISTANCE")):
+                response = str(distance)
+            else:
+                response = "INVALID"
+                
+            #Format Response    
+            response = "@" + response + "@\n"
+            
+            #Respond
+            print('Sending: ' + response)
+            conn.send(response.encode("utf-16"))
+            
+    print("Connection Closed")
 
 main()
